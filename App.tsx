7ca935fbbec9af5f9f99c276b3a14506cb1bf4e0
erik-ferreira <erikdesouzagf@gmail.com>
2024-02-05 23:32:20 -0300

    add nativewind and configs

--- App.tsx
+++ App.tsx
@@ -1,20 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar"
+import { Text, View } from "react-native"
+import { useFonts, Orbitron_400Regular } from "@expo-google-fonts/orbitron"
+import { Rajdhani_400Regular } from "@expo-google-fonts/rajdhani"
+import { JetBrainsMono_400Regular } from "@expo-google-fonts/jetbrains-mono"
+import { NativeWindStyleSheet } from "nativewind"
+
+NativeWindStyleSheet.setOutput({
+  default: "native",
+})
 
 export default function App() {
+  let [fontsLoaded, fontError] = useFonts({
+    Orbitron_400Regular,
+    Rajdhani_400Regular,
+    JetBrainsMono_400Regular,
+  })
+
+  if (!fontsLoaded && !fontError) {
+    return null
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-emerald-500 font-bold text-2xl text-center mb-10">
+        Open up App.js to start working on your app!
+      </Text>
+      <Text className="text-emerald-500 font-bold text-2xl text-center">
+        Open up App.js to start working on your app!
+      </Text>
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./App.tsx", "./src/**/*.tsx"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Orbitron_400Regular"],
+      },
+    },
+  },
+  plugins: [],
+}
