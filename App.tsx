267e10d25dd46f082f1517f940fc32e9843f0d88
erik-ferreira <erikdesouzagf@gmail.com>
2024-02-07 22:49:48 -0300

    add styled components and config fonts

--- App.tsx
+++ App.tsx
@@ -1,20 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar"
+import { StyleSheet, Text, View } from "react-native"
+import {
+  Rajdhani_400Regular,
+  Rajdhani_700Bold,
+} from "@expo-google-fonts/rajdhani"
+import {
+  useFonts,
+  Orbitron_400Regular,
+  Orbitron_600SemiBold,
+} from "@expo-google-fonts/orbitron"
+import {
+  JetBrainsMono_400Regular,
+  JetBrainsMono_700Bold,
+} from "@expo-google-fonts/jetbrains-mono"
+
+import { Label } from "./styles"
 
 export default function App() {
+  let [fontsLoaded, fontError] = useFonts({
+    Orbitron_400Regular,
+    Orbitron_600SemiBold,
+    Rajdhani_400Regular,
+    Rajdhani_700Bold,
+    JetBrainsMono_400Regular,
+    JetBrainsMono_700Bold,
+  })
+
+  if (!fontsLoaded && !fontError) {
+    return null
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Label>Open up App.tsx to start working on your app!</Label>
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
-});
+})
---
+++ styles.ts
@@ -0,0 +1,6 @@
+import styled from "styled-components/native"
+
+export const Label = styled.Text`
+  color: red;
+  font-family: "Orbitron_400Regular";
+`
