0153b792201d3ce60516edeb771ccae8cac1aaaf
erik-ferreira <erikdesouzagf@gmail.com>
2024-02-08 23:11:13 -0300

    complete component Button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,11 +1,27 @@
+import { useTheme } from "styled-components/native"
+import { ActivityIndicator, TouchableOpacityProps } from "react-native"
+
 import { ButtonContainer, TextButtonContainer } from "./styles"
 
-interface ButtonProps {}
+interface ButtonProps extends TouchableOpacityProps {
+  label?: string
+  loading?: boolean
+}
 
-export function Button({ ...rest }: ButtonProps) {
+export function Button({
+  label = "Salvar",
+  loading = false,
+  ...rest
+}: ButtonProps) {
+  const theme = useTheme()
+
   return (
-    <ButtonContainer>
-      <TextButtonContainer>Salvar</TextButtonContainer>
+    <ButtonContainer activeOpacity={0.7} {...rest}>
+      {loading ? (
+        <ActivityIndicator color={theme.colors.slate[100]} />
+      ) : (
+        <TextButtonContainer>{label}</TextButtonContainer>
+      )}
     </ButtonContainer>
   )
 }
