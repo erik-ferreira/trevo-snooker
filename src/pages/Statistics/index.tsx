8c075dcdd36a81973183751a9fb37ede3735f747
erik-ferreira <erikdesouzagf@gmail.com>
2024-02-10 18:58:59 -0300

    create switch preview mode table

--- src/pages/Statistics/index.tsx
+++ src/pages/Statistics/index.tsx
@@ -1,11 +1,38 @@
-import { Container, Label } from "./styles"
+import { useState } from "react"
+import { useTheme } from "styled-components/native"
 
-interface StatisticsProps {}
+import { Icon } from "@/components/Icon"
 
-export function Statistics({ ...rest }: StatisticsProps) {
+import { Container, ContentTitle, Title, ButtonPreviewMode } from "./styles"
+
+export function Statistics() {
+  const { colors } = useTheme()
+
+  const [previewMode, setPreviewMode] = useState<"vertical" | "horizontal">(
+    "horizontal"
+  )
+  const isSelectedHorizontal = previewMode === "horizontal"
+  const isSelectedVertical = previewMode === "vertical"
+
   return (
     <Container>
-      <Label>Statistics</Label>
+      <ContentTitle>
+        <Title>Visualizar tabela no formato:</Title>
+
+        <ButtonPreviewMode
+          isSelected={isSelectedHorizontal}
+          onPress={() => setPreviewMode("horizontal")}
+        >
+          <Icon name="RectangleHorizontal" color={colors.slate[200]} />
+        </ButtonPreviewMode>
+
+        <ButtonPreviewMode
+          isSelected={isSelectedVertical}
+          onPress={() => setPreviewMode("vertical")}
+        >
+          <Icon name="RectangleVertical" color={colors.slate[200]} />
+        </ButtonPreviewMode>
+      </ContentTitle>
     </Container>
   )
 }
--- src/pages/Statistics/styles.ts
+++ src/pages/Statistics/styles.ts
@@ -7,8 +7,37 @@ export const Container = styled.SafeAreaView`
   background-color: ${(props) => props.theme.colors.page};
 `
 
-export const Label = styled.Text`
-  color: red;
-  font-family: ${(props) => props.theme.fonts.orbi.normal};
-  font-size: 24px;
+export const ContentTitle = styled.SafeAreaView`
+  flex-direction: row;
+  align-items: center;
+  gap: 8px;
+`
+
+export const Title = styled.Text`
+  color: ${(props) => props.theme.colors.slate[400]};
+  font-family: ${(props) => props.theme.fonts.orbi.semiBold};
+  font-size: 18px;
+`
+
+interface ButtonPreviewModeProps {
+  isSelected?: boolean
+}
+
+export const ButtonPreviewMode = styled.TouchableOpacity.attrs(() => ({
+  activeOpacity: 0.7,
+}))<ButtonPreviewModeProps>`
+  width: 32px;
+  height: 32px;
+
+  border-radius: 4px;
+  border: 1px solid
+    ${(props) =>
+      !props.isSelected ? props.theme.colors.slate[400] : "transparent"};
+  background-color: ${(props) =>
+    props.isSelected ? props.theme.colors.blue[800] : "transparent"};
+
+  align-items: center;
+  justify-content: center;
+
+  transition: background-color 2s linear;
 `
